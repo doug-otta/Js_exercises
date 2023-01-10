5464528678772839817145cf608fc5ab11508f10
Douglas Otta <douglasottabezerra@gmail.com>
2023-01-10 18:04:46 -0300

    Delete ex015 directory

--- ex015/script.js
+++
@@ -1,6 +0,0 @@
-function calcIntPart(){
-    let intPart = Number(document.querySelector('input#intPart').value)
-    let res = document.querySelector('div#res')
-
-    res.innerHTML = `A parte inteira do valor ${intPart} é: ${Math.trunc(intPart)}`
-}
